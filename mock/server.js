691b141c344c2927073667695feb4f60e2dc28e7
zhulidan <ZLD_company@163.com>
2019-06-28 18:14:41 +0800

    库存

--- mock/server.js
+++ mock/server.js
@@ -8,7 +8,7 @@ function readLeftVal(cb) {//用来读取数据的
     //fs.readFile为异步的，所以在调用时需要传递回调函数，当读取数据后，在执行callback
     fs.readFile('./mock/json/leftNav.json', 'utf8', function (err, data) {
         if (err || !data) {
-          console.log(err,data)
+            console.log(err, data)
             cb(data);//如果有错误 或者文件没长度，就是空数组
         } else {
             cb(JSON.parse(data));//将读出来的内容转化成对象
@@ -16,11 +16,11 @@ function readLeftVal(cb) {//用来读取数据的
     })
 }
 //获取库存中的数据列表
-function readStockList(cb){
-    fs.readFile('./mock/json/stockData.json','utf8',function(err,data){
-        if(err || !data){
+function readStockList(cb) {
+    fs.readFile('./mock/json/stockData.json', 'utf8', function (err, data) {
+        if (err || !data) {
             cb(data);
-        }else{
+        } else {
             cb(JSON.parse(data))
         }
     })
@@ -37,16 +37,16 @@ http.createServer((req, res) => {//创建一个服务
 
     let { pathname, query } = url.parse(req.url);// 解析路径
     if (pathname === '/leftData') {
-      readLeftVal(function (navs) {
+        readLeftVal(function (navs) {
             // let hot = navs.reverse().slice(0, 6);//将获取的数据倒序，并截取前6个
             res.setHeader('Content-Type', 'application/json;charset=utf8');
             res.end(JSON.stringify(navs))
         })
         return;
     }
-    if(pathname === '/stockList'){
-        readStockList(function(datas){
-            res.setHeader('Content-Type','application/json;charset=utf8');
+    if (pathname === '/stockList') {
+        readStockList(function (datas) {
+            res.setHeader('Content-Type', 'application/json;charset=utf8');
             res.end(JSON.stringify(datas));
         })
     }
--- src/components/stockPurchase.vue
+++ src/components/stockPurchase.vue
@@ -1,24 +1,50 @@
 <template>
-    <div>
-        <Header>果子侠客 - 进货</Header>
-        <div class="content">
-          <Left></Left>
-        </div>
+  <div>
+    <Header>果子侠客 - 进货</Header>
+    <div class="content">
+      <Search :inputPlaceholder="inputPlaceholder"></Search>
+      <Table :menu="tableMenu" :tableList="tableData"></Table>
     </div>
+  </div>
 </template>
 <script>
-import Header from '../base/Header'
-import Left from '../base/Left'
+import Header from "../base/Header";
+import Table from "../base/Table";
+import Search from "../base/search";
 export default {
-    name: 'stockPurchase',
-    components:{
-        Header,
-        Left
-    }
-}
+  name: "stockPurchase",
+  components: {
+    Header,
+    Table,
+    Search
+  },
+  data() {
+    return {
+      inputPlaceholder: "搜索产品名称",
+      tableMenu: [
+        {
+          name: "订单编号",
+          field: "type"
+        },
+        {
+          name: "订单名称",
+          field: "name"
+        },
+        {
+          name: "订单日期",
+          field: "product"
+        },
+        {
+          name: "操作",
+          field: "supplier"
+        }
+      ],
+      tableData: []
+    };
+  }
+};
 </script>
 
 <style>
-
 </style>
 
